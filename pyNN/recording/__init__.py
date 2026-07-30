3948c25826e2cc6c67c823e73fd1348d765a01c2
Tibor Rozsa <tibor.rozsa@tutanota.com>
2026-07-30 14:06:13 +0200

    Ensure all MPI ranks query recording times

--- pyNN/recording/__init__.py
+++ pyNN/recording/__init__.py
@@ -348,6 +348,8 @@ class Recorder(object):
                 ids = sorted(self.filter_recorded(variable, filter_ids))
                 signal_array, times_array = self._get_all_signals(variable, ids, clear=clear)
                 mpi_node = self._simulator.state.mpi_rank  # for debugging
+                t_stop = self._simulator.state.t * pq.ms  # must run on all MPI nodes
+                current_time = self._simulator.state.t * pq.ms  # must run on all MPI nodes
                 if signal_array.size > 0:
                     # may be empty if none of the recorded cells are on this MPI node
                     units = self.population.find_units(variable)
@@ -398,9 +400,7 @@ class Recorder(object):
                             signal.segment = segment
                     else:
                         t_start = self._recording_start_time
-                        t_stop = self._simulator.state.t * pq.ms
                         sampling_period = self.sampling_interval * pq.ms
-                        current_time = self._simulator.state.t * pq.ms
                         signal = neo.AnalogSignal(
                             signal_array,
                             units=units,
